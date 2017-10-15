36d9a011b93cf4f627cae499a793a1eb4914218b
puroh <diego683@gmail.com>
2017-10-14 21:05:57 -0500

    añadido tb y correccion de errores

---
+++ control_tb.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 14.10.2017 19:53:13
+// Design Name: 
+// Module Name: control_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module control_tb(
+    input intruccion,
+    output RegDest,
+    output SaltoCond,
+    output LeerMem,
+    output MenmaReg,
+    output ALUOp,
+    output EscrMem,
+    output FuenteALU,
+    output escrReg
+    );
+endmodule
---
+++ memoria_datos_tb.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 14.10.2017 19:59:43
+// Design Name: 
+// Module Name: memoria_datos_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module memoria_datos_tb(
+    input EscrMem,
+    input outALU,
+    input Datain,
+    input LeerMem,
+    output Dataout
+    );
+endmodule
